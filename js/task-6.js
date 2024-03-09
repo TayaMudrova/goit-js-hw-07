4a1247b6c4538a3f6f964943becd820f94d083b7
Taya Mudrova <mudrova.taisiia@gmail.com>
2024-03-09 20:41:48 +0200

    task-6 with css done

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,42 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnCreate = document.querySelector('[data-create]');
+const btnDestroy = document.querySelector('[data-destroy]');
+const amount = document.querySelector("input[type='number']");
+const boxes = document.querySelector('#boxes');
+
+btnCreate.addEventListener('click', () => {
+  if (amount.value <= 100 && amount.value >= 1) {
+    destroyBox();
+    addBox();
+    amount.value = '';
+  } else {
+    alert('Please enter a number between 1 and 100');
+    amount.value = '';
+  }
+});
+
+function addBox() {
+  boxes.insertAdjacentHTML('afterbegin', createBoxes(amount));
+}
+
+function createBoxes(amount) {
+  let code = [];
+  let size = 0;
+  for (let i = 0; i < amount.value; i++) {
+    code.push(
+      `<div id="boxes${i}" style="height: ${size}px; width: ${size}px; background-color: ${getRandomHexColor()};"></div>`
+    );
+    size += 10;
+  }
+  return code.join('');
+}
+
+btnDestroy.addEventListener('click', destroyBox);
+
+function destroyBox() {
+  const boxContainer = document.querySelector('#boxes');
+  boxes.innerHTML = '';
+}
